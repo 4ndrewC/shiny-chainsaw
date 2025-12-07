54b693719c3508c1fe44f18d07646ef686c44227
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2025-12-07 00:06:20 -0600

    Update mat_mul.sv

--- mat_mul.sv
+++ mat_mul.sv
@@ -1,54 +1,44 @@
-// module mat_mul #(
-//     parameter int N = 16,       
-//     parameter int W = 16        
-// )(
-//     input  logic signed [W-1:0] mat1 [N][N],  
-//     input  logic signed [W-1:0] mat2 [N],     
-//     input  logic signed [W-1:0] bias [N],     
-//     output logic signed [W-1:0] out  [N]      
-// );
-
-//     localparam int ACC_W = 2*W + $clog2(N);
-
-//     integer i, j;
-//     logic signed [ACC_W-1:0] acc;
-
-//     // always_comb begin
-//     always @* begin
-//         for (i = 0; i < N; i++) begin
-//             acc = '0;
-//             for (j = 0; j < N; j++) acc += mat1[i][j] * mat2[j];
-//             acc += bias[i];
-//             out[i] = acc[W-1:0];
-//         end
-//     end
-
-// endmodule
 module mat_mul #(
-    parameter int N = 16,       
+    parameter int N = 200,       
     parameter int W = 16        
 )(
-    input  logic signed [W-1:0] mat1 [N][N],  
-    input  logic signed [W-1:0] mat2 [N],     
-    input  logic signed [W-1:0] bias [N],     
-    output logic signed [W-1:0] out  [N]
+    input logic clk,
+    input  logic signed [N-1:0] mat1 [W][W],  
+    input  logic signed [N-1:0] mat2 [W],     
+    input  logic signed [N-1:0] bias [W],     
+    output logic signed [N-1:0] out  [W],
+    input logic done,
+    output logic flag
 );
 
 integer i, j;
-logic signed [15:0] mult;
-logic signed [15:0] sum;
+logic signed [N-1:0] mult;
+logic signed [N-1:0] sum;
 
-assign out[0] = 0;
+//assign out[0] = 0;
 
 always_comb begin 
-    // for(i = 0; i < 16; i ++) begin 
-    //     sum = 16'h0000;
-    //     for(j = 0; j < 16; j++) begin 
-    //         mult = mat1[i][j] * mat2[j];
-    //         sum += mult;
-    //     end
-    //     out[i] = (sum + bias[i]) > 0 ? (sum + bias[i]) : 16'h000;
-    // end
+//always_ff @(posedge clk) begin
+    if(done==0) begin
+         for(i = 0; i < W; i ++) begin 
+             sum = 0;
+             for(j = 0; j < W; j++) begin 
+                 mult = mat1[i][j] * mat2[j];
+                 sum += mult;
+             end
+             out[i] = (sum + bias[i]) > 0 ? (sum + bias[i]) : 0;
+    //        out[i] = sum+bias[i];
+    //         out[i] = 16'h1111;
+             $display("%d\n", out[i]);
+         end
+//         done = 1;
+        flag = 1;
+     end
+     else begin
+//     `  $display("flag reset\n");
+        flag = 0;
+     end
+     
     // out[0] = 0;
     // $display("%d\n", out[0]);
 end
